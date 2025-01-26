035ca2d400cc0d8554d78f737be4e4813ac258f9
Youssef Labouiti <labouiti.youssef@gmail.com>
2025-01-26 16:50:41 +0100

    Réseau sociaux

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./Footer.css";
+import { Mail, Phone, Facebook, Instagram, X } from "lucide-react";
+import logo from "../assets/logo.jpg";
 
 const Footer = () => (
   <footer className="footer">
-    <div className="footer-logo">Web Tech</div>
-    <div className="footer-content">
-      <div>Email : contact@webtech.com </div>
-      <div>Téléphone : +33 6 12 34 56 78 </div>
+    <div>
+      <img src={logo} alt="logo" className="footer-logo" />
+    </div>
+    <div className="footer-contact">
+      <div className="contact-item">
+        <Mail className="icon" /> contact@webtech.com
+      </div>
+      <div className="contact-item">
+        <Phone className="icon" /> +33 7 52 10 28 30
+      </div>
+    </div>
+    <div className="footer-socials">
+      <a
+        href="https://facebook.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-item"
+      >
+        <Facebook className="icon" />
+      </a>
+      <a
+        href="https://instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-item"
+      >
+        <Instagram className="icon" />
+      </a>
+      <a
+        href="https://twitter.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-item"
+      >
+        <X className="icon" />
+      </a>
     </div>
   </footer>
 );
